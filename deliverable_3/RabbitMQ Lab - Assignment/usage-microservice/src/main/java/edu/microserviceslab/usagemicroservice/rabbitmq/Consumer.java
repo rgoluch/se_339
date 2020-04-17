a6bc18d10a6660ba20209a8a74e5323db339fc53
rgoluch <rgoluch@iastate.edu>
2020-04-16 21:34:32 -0500

    onMessage, we Consume a UseageService

--- deliverable_3/RabbitMQ Lab - Assignment/usage-microservice/src/main/java/edu/microserviceslab/usagemicroservice/rabbitmq/Consumer.java
+++ deliverable_3/RabbitMQ Lab - Assignment/usage-microservice/src/main/java/edu/microserviceslab/usagemicroservice/rabbitmq/Consumer.java
@@ -12,8 +12,34 @@ import java.io.IOException;
 
 //Make your modifications here. You may need to add more methods or a constructor.
 public class Consumer implements MessageListener {
+
+    private UseageService u;
+    private UsageStatisticRepo r;
+
+    Consumer(){
+        super();
+    }
+
+    Consumer(UseageService u){
+        this.u = u;
+    }
+
     @Override
     public void onMessage(Message message) {
 
+        JSONObject j = message.getBody();
+        ObjectMapper mapper = new ObjectMapper();
+        UseageStatisticRequest s = mapper.readValue(j, UseageStatisticRequest.class);
+
+        List<PidRequest> pid = s.getPids();
+
+        for (PidRequest p : pid){
+            UsageStatistic temp = new UsageStatistic();
+            temp.setVehicleId(s.getVehicleId());
+            temp.setPid(p);
+            temp.setData(p);
+
+            r.save(temp);
+        }
     }
 }
